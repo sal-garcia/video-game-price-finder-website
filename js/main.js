6cbc29051842f27916afe4304ecde795cfc947b7
sal garcia <salgarcia940@gmail.com>
2022-02-24 16:44:31 -0800

    used rem instead of px for small stuff, and swapped innerHTHML with DOM creations

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
 function reqListener() {
   // "this" is the xmlhttps request obj
+
   var gamesArr = JSON.parse(this.responseText);
   // the stringified raw data will be parsed into js code
-  var $main = document.querySelector('main');
+
+  var $homeContainer = document.querySelector('.home-container');
   for (const game of gamesArr) {
-    $main.innerHTML += `
-          <div class="game-info-rows row justify-content-space-around">
-        <img class="game-deal-image" src="${game.thumb}" alt="game">
-          <div class="column">
-            <div class="game-deal-width">
-              <h2 class="blue-title">${game.title}</h2>
-              <h2>${game.salePrice}</h2>
-            </div>
-          </div>
-        </div>
-`
-    ;
+    // dom creations
+    var $outerGameDiv = document.createElement('div');
+    $outerGameDiv.setAttribute('class', 'game-info-rows row justify-content-space-around');
+    $homeContainer.appendChild($outerGameDiv);
+
+    var $gameDealImg = document.createElement('img');
+    $gameDealImg.setAttribute('class', 'game-deal-image');
+    $gameDealImg.setAttribute('src', game.thumb);
+    $gameDealImg.setAttribute('alt', 'image');
+
+    $outerGameDiv.appendChild($gameDealImg);
+
+    var $columnDiv = document.createElement('div');
+    $columnDiv.setAttribute('class', 'column');
+
+    $outerGameDiv.appendChild($columnDiv);
+
+    var $gameDealBlock = document.createElement('div');
+    $gameDealBlock.setAttribute('class', 'game-deal-width');
+
+    $columnDiv.appendChild($gameDealBlock);
+
+    var $gameTitle = document.createElement('h2');
+    $gameTitle.setAttribute('class', 'blue-title');
+    $gameTitle.textContent = game.title;
+
+    $gameDealBlock.appendChild($gameTitle);
+
+    var $gameSalePrice = document.createElement('h2');
+    $gameSalePrice.textContent = game.salePrice;
+
+    $gameDealBlock.appendChild($gameSalePrice);
+
   }
 }
 
+// XMLHTttpResquest
 var oReq = new XMLHttpRequest();
 oReq.addEventListener('load', reqListener);
 oReq.open('GET', 'https://www.cheapshark.com/api/1.0/deals?pageSize=5&sortBy=Price');
 oReq.send();
+// XMLHTttpResquest
